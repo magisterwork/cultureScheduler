eb03e2d895c806b6d20296112ae7484fad3c4d06
SBT-Kudryashov-AA <sbt-kudryashov-aa.sbt@sberbank.ru>
2017-10-12 17:53:37 +0300

    skeleton

---
+++ src/main/java/org/spree/CultureschedulerApplication.java
@@ -0,0 +1,12 @@
+package org.spree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CultureschedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CultureschedulerApplication.class, args);
+	}
+}
---
+++ src/test/java/org/spree/CultureschedulerApplicationTests.java
@@ -0,0 +1,16 @@
+package org.spree;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CultureschedulerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
